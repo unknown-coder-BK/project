1e48f0a7f5b999e9b8e8debe4fe21dad43b64d59
ZhangBen <zb85760059>
2022-05-05 17:55:19 +0800

    add: test Dao类 报错Table 'newbee_mall_db.mall_user' doesn't exist

--- src/test/java/zb/mall/DaoTest.java
+++ src/test/java/zb/mall/DaoTest.java
@@ -1,10 +1,24 @@
 package zb.mall;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import zb.mall.core.dao.MallUserDao;
+import zb.mall.core.entity.vo.MallUser;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class DaoTest {
+    @Autowired
+    MallUserDao mallUserDao;
+
+    @Test
+    public void test(){
+        List<MallUser> mallUsers = mallUserDao.selectList(null);
+        System.out.println(mallUsers);
+    }
 }
