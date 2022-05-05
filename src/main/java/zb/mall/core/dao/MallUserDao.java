e23a95dce18889d8842d5a90f9ef5e7fc9c94405
ZhangBen <zb85760059>
2022-05-05 16:35:17 +0800

    add: 引入mybatis-plus 启动报错 -增加@Mapper注解

--- src/main/java/zb/mall/core/dao/MallUserDao.java
+++ src/main/java/zb/mall/core/dao/MallUserDao.java
@@ -1,8 +1,10 @@
 package zb.mall.core.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import zb.mall.core.entity.vo.MallUser;
 
+@Mapper
 public interface MallUserDao extends BaseMapper<MallUser> {
 
 }
