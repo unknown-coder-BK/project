2761bfffea2d6746ad7d34bf82fd42320bc321ce
ZhangBen <zb85760059>
2022-05-05 15:44:41 +0800

    add: Apache工具组件 commons-lang3

--- src/main/java/zb/mall/controller/register/RegisterController.java
+++ src/main/java/zb/mall/controller/register/RegisterController.java
@@ -1,9 +1,11 @@
 package zb.mall.controller.register;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import zb.mall.base.resp.R;
+import zb.mall.constans.Constants;
 import zb.mall.pojo.user.MallUserReq;
 
 import javax.servlet.http.HttpSession;
@@ -19,7 +21,13 @@ public class RegisterController {
     @RequestMapping(value = "register",method = RequestMethod.POST)
     @ResponseBody
     public R register(@RequestBody @Validated(value = {MallUserReq.Register.class}) MallUserReq req, HttpSession session){
-        return R.error("验证码错误");
+        String loginName = req.getLoginName();
+        String passWord = req.getPassWord();
+        String verifyCode = req.getVerifyCode();
+        String captchaCode = (String)session.getAttribute(Constants.MALL_VERIFY_CODE_KEY);
+        if(!StringUtils.equalsIgnoreCase(verifyCode,captchaCode)){
+            return R.error("验证码错误");
+        }
+        return null;
     }
-
 }
