49bb39afb1e8520a7ac35fa8c88e0ab90a65a46e
ZhangBen <zb85760059>
2022-05-05 16:08:07 +0800

    add: 引入spring-boot-test

---
+++ src/test/java/zb/mall/DaoTest.java
@@ -0,0 +1,8 @@
+package zb.mall;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class DaoTest {
+}
