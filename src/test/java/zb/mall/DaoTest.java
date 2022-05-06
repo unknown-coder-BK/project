fb4595f18a4fb992cadaa756412eb4d95d49c83a
ZhangBen <zb85760059>
2022-05-06 11:03:16 +0800

    add:DaoTest tb_newbee_mall_coupon mybatis-plus 接口

--- src/test/java/zb/mall/DaoTest.java
+++ src/test/java/zb/mall/DaoTest.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import zb.mall.core.dao.MallUserDao;
+import zb.mall.core.entity.vo.Coupon;
 import zb.mall.core.entity.vo.MallUser;
+import zb.mall.core.service.CouponService;
+import zb.mall.core.service.MallUserService;
 
 import java.util.List;
 
@@ -16,9 +19,28 @@ public class DaoTest {
     @Autowired
     MallUserDao mallUserDao;
 
+    @Autowired
+    MallUserService mallUserService;
+
+
+    @Autowired
+    CouponService couponService;
+
     @Test
-    public void test(){
+    public void test1(){
         List<MallUser> mallUsers = mallUserDao.selectList(null);
         System.out.println(mallUsers);
+    }
+
+    @Test
+    public void test2(){
+        List<MallUser> list = mallUserService.list(null);
+        System.out.println(list);
+    }
+
+    @Test
+    public void test3(){
+        List<Coupon> list = couponService.list(null);
+        System.out.println(list);
     }
 }
