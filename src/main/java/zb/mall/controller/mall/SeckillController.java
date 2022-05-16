7ff6aecc0c596224e9698dd5be9ad00ae9b8c634
ZhangBen <zb85760059>
2022-05-16 13:16:21 +0800

    add:引入redis

--- src/main/java/zb/mall/controller/mall/SeckillController.java
+++ src/main/java/zb/mall/controller/mall/SeckillController.java
@@ -1,15 +1,23 @@
 package zb.mall.controller.mall;
 
+import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("seckill")
 public class SeckillController {
+    @Resource
+    private RedisCache redisCache;
 
     @GetMapping("/list")
-    public String listPage(){
+    public String listPage(HttpServletRequest request, HttpServletResponse response, Model model){
         return "mall/seckill-list";
     }
 }
---
+++ src/main/java/zb/mall/redis/RedisCache.java
@@ -0,0 +1,24 @@
+package zb.mall.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisCache {
+
+    @Autowired
+    public RedisTemplate redisTemplate;
+
+    public <T> T getCacheObject(final String key) {
+        ValueOperations<String, T> operation = redisTemplate.opsForValue();
+        return operation.get(key);
+    }
+
+    public <T> void setCacheObject(final String key, final T value, final Integer timeout, final TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, value, timeout, timeUnit);
+    }
+}
